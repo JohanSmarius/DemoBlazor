00fe507972dfabb740f9ac87e40eb70707b273e2
Johan Smarius <jsmarius@hotmail.com>
2019-01-15 22:52:12 +0100

    Persons added

---
+++ DemoBlazor/Core.Domain/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Core.Domain
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Picture { get; set; }
+
+
+    }
+}
