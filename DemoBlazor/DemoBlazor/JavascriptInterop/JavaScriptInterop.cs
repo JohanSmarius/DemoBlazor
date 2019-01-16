9d03f106c67f86fa894e380357516686fbe3d878
Johan Smarius <jsmarius@hotmail.com>
2019-01-16 23:49:40 +0100

    Added JS Interop

---
+++ DemoBlazor/DemoBlazor/JavascriptInterop/JavaScriptInterop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace DemoBlazor.JavascriptInterop
+{
+    public static class JavaScriptInterop
+    {
+        public static Task<bool> Confirm(string message)
+        {
+            return JSRuntime.Current.InvokeAsync<bool>("helperfunction.confirm", message);
+        }
+    }
+}
